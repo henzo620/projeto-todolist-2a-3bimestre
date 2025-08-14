dbc174929efed9308be442f82110b83455a1b62f
henzo lucas rizzo de souza <henzinho620@gmail.com>
2025-08-14 12:39:48 -0300

    [feat] logo in app.js

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { Image, StyleSheet, Text, View } from 'react-native';
-import imgLogo from './assets/pngwing.com.png'
+import imgLogo from './assets/pngwing.com.png';
 export default function App() {
   return (
     <View style={styles.container}>
       <view>
-        <Image source={imgLogo}/>
+        <Image source={imgLogo} style={styles.logo}/>
       </view>
-      <Text>hello app do henzo</Text>
+      <Text>Henzo</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,11 +16,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  // },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
   logo: {
-    width: 200,
-    height: 200,}
+    width: 128, height: 128,
+  }
 });
