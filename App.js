eb587ef42a1e7c5bd39eaf1deaf79f6446ec34cb
henzo lucas rizzo de souza <henzinho620@gmail.com>
2025-08-21 11:58:01 -0300

    [feat]

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TextInput, View } from 'react-native';
 import imgLogo from './assets/pngwing.com.png';
 export default function App() {
+import { useState } from "react";
+import btnAdd from './assets/add button-.png';
+
+  const [tarefa, setTarefa] = useState("")
   return (
     <View style={styles.container}>
       <view>
         <Image source={imgLogo} style={styles.logo}/>
       </view>
       <Text>Henzo</Text>
+      <TextInput 
+      placeholder="entre com a tarefa" 
+      value='{tarefa}'
+      onChangeText={setTarefa}
+      />
       <StatusBar style="auto" />
     </View>
   );
