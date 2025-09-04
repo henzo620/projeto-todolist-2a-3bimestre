3e2f0d25f2f6aed094dc0b8926527adaa6dd6d42
henzo lucas rizzo de souza <henzinho620@gmail.com>
2025-09-04 11:51:59 -0300

    [feat] Add tarefas

--- App.js
+++ App.js
@@ -1,22 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, Text, TextInput, View } from 'react-native';
-import imgLogo from './assets/pngwing.com.png';
-export default function App() {
+import { StatusBar } from "expo-status-bar";
+import { Alert, Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import imgLogo from "./assets/pngwing.com.png";
+import btnAdd from "./assets/add button.png";
 import { useState } from "react";
-import btnAdd from './assets/add button-.png';
 
-  const [tarefa, setTarefa] = useState("")
+export default function App() {
+  const [tarefa, setTarefa] = useState("");
+  const [tarefas, setTarefas] = useState([])
+
+  const handleAdd = () => {
+    //Alert.alert(tarefa);
+    setTarefas([tarefa, ...tarefas])
+    setTarefa("");
+  };
+
+  //ja importei o flash list e agora tem que usar
+
   return (
     <View style={styles.container}>
-      <view>
-        <Image source={imgLogo} style={styles.logo}/>
-      </view>
-      <Text>Henzo</Text>
-      <TextInput 
-      placeholder="entre com a tarefa" 
-      value='{tarefa}'
-      onChangeText={setTarefa}
-      />
+      <View>
+        <Image source={imgLogo} style={styles.logo} />
+        <Text>todo list</Text>
+      </View>
+      <View style={styles.viewInput}>
+        <TextInput
+          placeholder="Add Tarefa"
+          value={tarefa}
+          onChangeText={setTarefa}
+        />
+        <TouchableOpacity onPress={handleAdd}>
+        <Image source={btnAdd} style={styles.botaoAdd} />
+        </TouchableOpacity>
+      </View>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -28,8 +44,22 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    padding: 10,
   },
+
   logo: {
-    width: 128, height: 128,
-  }
+    width: 128,
+    height: 128,
+  },
+
+  botaoAdd: {
+    width: 32,
+    height: 32,
+  },
+
+  viewInput: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "80%",
+  },
 });
